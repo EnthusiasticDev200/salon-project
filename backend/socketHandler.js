2e4cee5d1ef58f6f1434db999f8962fd68b5ec55
joshua makanjuola <joshuamakanjuola99@gmail.com>
2025-06-18 20:55:03 +0100

    Created customer dashboard and implemented customer join room logic in socketHandler.js

--- backend/socketHandler.js
+++ backend/socketHandler.js
@@ -18,6 +18,10 @@ function initSocket(server){
           socket.join(stylistUsername);
           console.log(`Socket ${socket.id} joined room: ${stylistUsername}`);
         });
+        socket.on("joinCustomerRoom", (customerUsername)=>{
+            socket.join(customerUsername);
+            console.log(`Socket ${socket.id} joined customer room: ${customerUsername};` )
+        })
         //stylist response to appointment
         socket.on("stylistResponse", async ({ appointmentId, status, customerUsername }) => {
             try{
@@ -34,7 +38,7 @@ function initSocket(server){
                 );
                 console.log(`Appointment ${appointmentId} updated to status '${status}'`);
                 // (Optional) Notify the customer
-                io.to(`customer_${customerUsername}`).emit("AppointmentStatusUpdated", {
+                io.to(`customer_${customerUsername}`).emit("appointmentStatusUpdated", {
                     appointmentId,
                     status
                 });
