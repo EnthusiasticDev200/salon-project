d9c6edbb26839e7231a91f6b40367d36329f12c4
joshua makanjuola <joshuamakanjuola99@gmail.com>
2025-07-02 21:52:49 +0100

    Added route for admins, customers and stylists

--- express.js
+++ express.js
@@ -4,14 +4,15 @@ const cors = require("cors")
 const cookieParser = require('cookie-parser')
 const authRoutes = require('./backend/routes/authRoutes')
 const http = require('http')
+const path = require('path')
 const app = express()
 const server = http.createServer(app)
 const {initSocket} = require('./backend/socketHandler')
 
 //cors set-up
 app.use(cors({
-    "origin" : "http://localhost:5173",
-    "credentials" : true
+    origin : "http://localhost:3100", //change to Vite later
+    credentials : true
 }))
 
 //middleware
@@ -21,6 +22,28 @@ app.use(cookieParser());
 //troutes
 app.use('/auth',authRoutes) //all endpointmust start with auth
 
+app.get('/salon/admin/login', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/frontend/admins/loginAdmin.html'))
+})
+app.get('/salon/admin/changepw', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/frontend/admins/changeAdminPassword.html'))
+})
+app.get('/salon/customer/login', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/frontend/customers/loginCustomer.html'))
+})
+app.get('/salon/customer/dashboard', (req, res)=>{
+    res.sendFile(path.join(__dirname,'/frontend/customers/customerDashboard.html'))
+})
+app.get('/salon/stylist/login', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/frontend/stylists/loginStylist.html'))
+})
+app.get('/salon/stylist/dashboard', (req, res)=>{
+    res.sendFile(path.join(__dirname,'/frontend/stylists/stylistDashboard.html'))
+})
+
+app.get('/salon/appointment', (req, res)=>{
+    res.sendFile(path.join(__dirname, "/frontend/appointments/createAp.html"))
+})
 //Socket set-up
 initSocket(server); // initializing initSocket
 
