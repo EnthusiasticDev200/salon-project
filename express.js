e96199c21f711033399545c577e6cd70e25437e9
joshua makanjuola <joshuamakanjuola99@gmail.com>
2025-08-22 15:13:30 +0100

    Added error handler in express file

--- express.js
+++ express.js
@@ -1,27 +1,29 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express')
-const cors = require("cors")
-const cookieParser = require('cookie-parser')
-const helmet = require('helmet')
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
 
-const http = require('http')
+const http = require("http");
 
-const authRoutes = require('./backend/routes/authRoutes')
-const {initSocket} = require('./backend/socketHandler')
-const logger = require("./utils/logger")
+const authRoutes = require("./backend/routes/authRoutes");
+const { initSocket } = require("./backend/socketHandler");
+const logger = require("./utils/logger");
 
-const app = express()
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 
 //cors set-up
-app.use(cors({
-    origin : "http://localhost:3100", //change to 5173 for vite
-    credentials : true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:3100", //change to 5173 for vite
+    credentials: true,
+  })
+);
 
 //middleware
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Strict helmet settings for live environment
   app.use(helmet());
 } else {
@@ -38,42 +40,51 @@ if (process.env.NODE_ENV === 'production') {
   );
 }
 
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser());
 
 //routes
-app.use('/auth',authRoutes) 
+app.use("/auth", authRoutes);
 
 //landing page route
-app.get('/', (req,res)=>{
-    res.send("Welcome to KhleanCutz Salon")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to KhleanCutz Salon");
+});
 
-// catch unavailable route
-app.use('',(req, res)=>{
-    res.status(401).json({message:'Route not found'})
-})
-    
+
 //Socket set-up
-initSocket(server); 
+initSocket(server);
+
 
 // Render deployment
 app.get("/healthz", (req, res) => res.send("OK"));
 
-const PORT = process.env.APP_PORT
-server.listen(PORT, ()=>{
-    logger.info(`Server started on http://localhost:${PORT}`);
-    console.log(`Server is running on http://localhost:${PORT}`)
- 
-process.on('uncaughtException', (err)=>{
+// catch unavailable route
+app.use('', (req, res)=>{
+    res.status(404).json({ message: "Route not found" });
+})
+// Error handler
+app.use((err, req, res, next) => {
+  if (err) {
+    console.log(err.stack);
+    res
+      .status(500)
+      .json({ message: `Internal Server Error` });
+  }
+});
+
+const PORT = process.env.APP_PORT;
+server.listen(PORT, () => {
+  logger.info(`Server started on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
+
+  process.on("uncaughtException", (err) => {
     logger.error(`Uncaught Exception: ${err.message}`, { stack: err.stack });
     process.exit(1); // stop app to avoid broken state
-    })
-//catch uncaught promise
-process.on('unhandledRejection', (err) => {
+  });
+  //catch uncaught promise
+  process.on("unhandledRejection", (err) => {
     logger.error(`Unhandled Rejection: ${err.message || err}`);
     process.exit(1);
-    });
-})
-
-
+  });
+});
