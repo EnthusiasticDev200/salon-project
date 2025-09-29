424b8be306913687c1f66d923c367bd7adfef9f4
joshua makanjuola <joshuamakanjuola99@gmail.com>
2025-09-30 00:45:33 +0100

    Restructured redis logic

--- backend/redis.js
+++ backend/redis.js
@@ -3,7 +3,7 @@ const Redis = require('ioredis')
 
 dotenv.config()
 
-const prodRedis = process.env.RENDER_REDIS_PUB_URL ||process.env.REDIS_URL
+const prodRedis = process.env.REDIS_URL
 
 let redis;
 if(process.env.NODE_ENV === 'development' ){
@@ -11,7 +11,9 @@ if(process.env.NODE_ENV === 'development' ){
   redis = new Redis({ host: '127.0.0.1', port: 6379 })
 } else{
   console.log('Redis runnning on PRODUCTION!')
-  redis = new Redis(prodRedis)
+  redis = new Redis(prodRedis, {
+    tls: prodRedis?.startsWith('rediss://') ? {} : undefined // hits redis internal url
+  })
 }
   
 // Event listeners
