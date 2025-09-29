84973af19379e2b69e8ddd63796eff8905b60eaf
joshua makanjuola <joshuamakanjuola99@gmail.com>
2025-09-29 15:07:19 +0100

    Fixed redis production set up

--- backend/redis.js
+++ backend/redis.js
@@ -7,8 +7,11 @@ let redis;
 if(process.env.NODE_ENV === 'development' ){
   console.log('Redis runnning on DEVELOPMENT!')
   redis = new Redis({ host: '127.0.0.1', port: 6379 })
-} process.env.RENDER_REDIS_PUB_URL
-
+} else{
+  console.log('Redis runnning on PRODUCTION!')
+  redis = new Redis(process.env.RENDER_REDIS_PUB_URL)
+}
+  
 // Event listeners
 redis.on('ready', () => console.log('Redis connected'))
 redis.on('error', (err) => console.log('Redis error:', err.stack))
