492d0daedd1cbe21f979f5eaa8877271d4edab01
joshua makanjuola <joshuamakanjuola99@gmail.com>
2025-10-03 12:34:39 +0100

    Refactored worker file

--- utils/worker.js
+++ utils/worker.js
@@ -5,12 +5,13 @@ const { sendOtpEmail } = require('./mailer')
 
 
 // set-up redis connection (local and prod.)
-
-const connection = new Redis('127.0.0.1:6379',{
-   maxRetriesPerRequest : null 
-})
-
-
+let connection;
+if(process.env.NODE_ENV === 'production' ) {
+    connection = new Redis(process.env.REDIS_URL,{maxRetriesPerRequest : null })
+}else{
+    console.log('Worker on DEVELOPMENT')
+    connection = new Redis('127.0.0.1:6379',{maxRetriesPerRequest : null })
+}
 
 // set up queue for otp-emails. Queue takes only connection as obj
 const otpQueue = new Queue('otp-emails', { connection })
