44b24489c42611dd5dc8714f0fad1db2ca87df91
joshua makanjuola <joshuamakanjuola99@gmail.com>
2025-10-15 00:19:34 +0100

    Fixed error

--- backend/cors.js
+++ backend/cors.js
@@ -1,8 +1,8 @@
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://khleancutz-salon.onrender.com", //backend
-  "https//khleancutz.vercel.app" // frontend
+  "https://khleancutz-salon.onrender.com", 
+  "https://khleancutz.vercel.app" 
 ];
 
 module.exports = allowedOrigins;
