97391eb866976e12b8220e34d7fcaf43050b86c7
joshua makanjuola <joshuamakanjuola99@gmail.com>
2025-11-08 14:39:04 +0100

    Emptied imageUpload middleware

--- middlewares/imageFile.js
+++
@@ -1,8 +0,0 @@
-
-const multer = require('multer');
-
-const upload = multer({ dest: 'uploads/' });
-
-console.log("upload from Midware", upload)
-
-module.exports = upload.single('file'); // <-- must match Postman key
